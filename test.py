c0c370e02ccd471396a2790c7cdf1a56fdcd6ebf
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2022-12-12 16:41:01 +0300

    tkinter window

---
+++ test.py
@@ -0,0 +1,27 @@
+from tkinter import *
+window = Tk()
+window.geometry("620x820")
+window.title("tomiii")
+icon=PhotoImage(file='botdog.png')
+window.iconphoto(True,icon)
+window.config(background="#411107")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
