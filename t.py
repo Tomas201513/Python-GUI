87e66f1c51e0c26b603465553f68690cb7cd2073
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2022-12-22 15:04:50 +0300

    connection checker done

--- t.py
+++ t.py
@@ -1,5 +1,25 @@
-import time
-while True:
-    print("x")
-    time.sleep(3)
-    
+import time 
+from time import sleep   
+import urllib.request
+from colorama import Fore, Back, Style
+
+def connect():
+        try:
+            urllib.request.urlopen('http://google.com') #Python 3.x
+            return True
+        except:
+            return False
+
+for i in range(0,100):
+    # try:
+        if connect():
+            print(Back.GREEN, Fore.YELLOW)
+            print("good")
+            sleep(3)
+        else:
+            print(Back.RED, Fore.YELLOW)
+            print("bad")
+            sleep(3)
+    # except:
+    #     print("not working")
+# print("not working")
